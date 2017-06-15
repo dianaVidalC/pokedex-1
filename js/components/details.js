ce81a9f3d8e2c19e4e658dc29777b201bcd0912a
Miriam Peralta <miiriampc@gmail.com>
2017-06-15 18:07:57 -0500

    styles grid pokemons

--- js/components/details.js
+++ js/components/details.js
@@ -1,6 +1,10 @@
 "use strict";
 const PokemonDetails = (update)=>{
     const modalPokemon = $('<div class="modal-pokemon"></div>');
+    const modalContent = $('<div class="modal-content"></div>');
+    const divPokemon = $('<div class="poke-left"></div>');
+    const divPokemon = $('<div class="poke-right"></div>');
+
 
     update();
     return modalPokemon;
