7d92b1434cf6af25cdbaecc384221baef9afae2e
miriampc <miiriampc@gmail.com>
2017-06-19 01:40:34 -0500

    add styles pokedex

--- js/components/details.js
+++ js/components/details.js
@@ -1,9 +1,9 @@
 "use strict";
 const PokemonDetails = (pokemon)=>{
-  let ability = '',types = '', damages='';
+    let ability = '',types = '', damages='';
     const divContainer = $('<div class="container flex-container"></div>');
     const modalContent = $('<div class="modal-content flex-container"></div>');
-    const namePokemon = $(`<h2>${pokemon.name}</h2>`);
+    const modalHeader = $(`<div class="modal-header"><h2>${pokemon.name}</h2></div>`);
     const closeModal = $('<a href="#" class="close-modal">&times;</a>');
     const divData = $('<div class="data flex-container"></div>');
     const divPokemon = $(`<div class="poke-image">${state.pokemonComponent}</div>`);
@@ -12,17 +12,18 @@ const PokemonDetails = (pokemon)=>{
     const features = $('<div class ="features flex-container"></div>');
     const height = $(`<div><h5>Altura:</h5><p>${pokemon.height/10} m</p></div>`);
     const weight = $(`<div><h5>Peso:</h5><p>${pokemon.weight/10} kg</p></div>`);
-    const genero = $(`<div><h5>Sexo:</h5><p>s</p></div>`);
+    const genero = $(`<div><h5>Sexo:</h5><p>female</p></div>`);
     const category = $(`<div><h5>Categoria:</h5><p>${pokemon.category}</p></div>`);
     pokemon.abilities.forEach((e)=>{ability += `<p>${e}</p>`;});
     const divAbility = $(`<div><h5>Habilidad:</h5>${ability}</div>`);
     pokemon.type.forEach((e)=>{types += `<span class="${e}">${e}</span>`; });
     const type = $(`<div class ="type"><h4>Tipo:</h4>${types}</div>`);
     pokemon.weakness.forEach((e)=>{damages += `<span class="${e}">${e}</span>`;});
-    const weakness = $(`<div class ="weakness"><h4>Debilidad:</h4>${damages}</div>`);
+    const weaknessTitle =$('<h4>Debilidad:</h4>');
+    const weakness = $(`<div class ="weakness flex-container">${damages}</div>`);
 
-    modalContent.append(namePokemon);
-    modalContent.append(closeModal);
+    modalHeader.append(closeModal);
+    modalContent.append(modalHeader);
     divDetails.append(description);
     features.append(height);
     features.append(weight);
@@ -31,6 +32,7 @@ const PokemonDetails = (pokemon)=>{
     features.append(divAbility);
     divDetails.append(features);
     divDetails.append(type);
+    divDetails.append(weaknessTitle);
     divDetails.append(weakness);
     divData.append(divPokemon);
     divData.append(divDetails);
